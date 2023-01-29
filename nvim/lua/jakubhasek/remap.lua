1225ea230d72ec255265e0d0e34cf7bb702dd136
Jakub Hašek <28836407+kubahasek@users.noreply.github.com>
2023-01-29 14:06:27 +0100

    added remap for lazygit

--- nvim/lua/jakubhasek/remap.lua
+++ nvim/lua/jakubhasek/remap.lua
@@ -31,3 +31,4 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>")
